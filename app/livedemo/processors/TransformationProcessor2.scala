9419700fb476c594dc86c4c8a48d8dca24d37f75
hirata-k <hirata.k@shanon.co.jp>
2013-05-28 02:06:54 +0900

    update

--- app/livedemo/processors/TransformationProcessor2.scala
+++ app/livedemo/processors/TransformationProcessor2.scala
@@ -3,14 +3,17 @@ package livedemo.processors
 import org.apache.camel.{Exchange, Processor}
 import livedemo.errorhandling.AbsurdException
 import livedemo.beans.TransformationBean
+import org.apache.log4j.Logger
 
 class TransformationProcessor2 extends Processor {
 
+  val logger = Logger.getLogger(this.getClass.getName)
+  
   def process(exchange: Exchange) {
 
-    val str = exchange.getIn(classOf[TransformationBean])
+    val str = exchange.getIn()
     //val str = "Oh oh, I will die now :-("
-    println(str)
+    logger info str
     str
   }
 }
--- app/livedemo/routes/RuntimeExceptionRoute.scala
+++ app/livedemo/routes/RuntimeExceptionRoute.scala
@@ -5,13 +5,14 @@ import livedemo.processors.TransformationProcessor
 import livedemo.processors.TransformationProcessor2
 import livedemo.errorhandling.ErrorLog
 import livedemo.beans.TransformationBean
+import livedemo.beans.TransformationBean2
 
 class RuntimeExceptionRoute extends RouteBuilder with ErrorLog {
 
 
 //  "direct:goToException" bean(new TransformationBean) process(new TransformationProcessor)
 
-  "direct:goToException" bean(classOf[TransformationBean]) process(new TransformationProcessor2) process(new TransformationProcessor2)
+  "direct:goToException" bean(classOf[TransformationBean]) process(new TransformationProcessor2) bean(classOf[TransformationBean2])
 
   
   // alternative for calling a bean: bean(classOf[TransformationBean]
